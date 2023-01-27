5ff3c3b759cc16759e8223da3191142c3d3990bd
jbezanilla <joaquinbezanilla@gmail.com>
2023-01-27 20:51:40 +0100

    added check for neighbors function

---
+++ checkForNeighbors.js
@@ -0,0 +1,19 @@
+import { generateArray } from "./generateArray.js";
+
+export const checkForNeighbors = (arr, x, y) => {
+  let currentCell = 0;
+  for (let k = -1; k <= 1; k++) {
+    for (let o = -1; o <= 1; o++) {
+      currentCell += arr[y + k][x + o];
+    }
+  }
+
+  return currentCell - arr[x][y];
+};
+
+const arr1 = [
+  [0, 0, 0],
+  [0, 1, 0],
+  [0, 0, 0],
+];
+console.log(checkForNeighbors(arr1, 1, 1));
