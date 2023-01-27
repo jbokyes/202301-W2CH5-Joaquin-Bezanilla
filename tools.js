7003cd834a580674e8a6d4fd3cea017c88bdee42
jbezanilla <joaquinbezanilla@gmail.com>
2023-01-27 19:27:09 +0100

    added table with generateArray (of arrays) function

--- tools.js
+++ tools.js
@@ -1,2 +1,15 @@
-const sample = true;
-console.log(sample);
+export const generateArray = (w) => {
+  const arr = new Array(w);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(w);
+  }
+
+  for (let k = 0; k < arr.length; k++) {
+    for (let o = 0; o < arr[k].length; o++) {
+      arr[k][o] = "_";
+    }
+  }
+
+  console.table(arr);
+  return arr.length;
+};
