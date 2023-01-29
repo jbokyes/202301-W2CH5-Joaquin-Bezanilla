d9860c339ae603001c08e203df04c177b6a9abac
jbezanilla <joaquinbezanilla@gmail.com>
2023-01-29 11:22:53 +0100

    added array randomizer as a separate, non testeable function

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 import { generateArray } from "./generateArray.js";
 import { checkForNeighbors } from "./checkForNeighbors.js";
 import { checkForWalls } from "./checkForWalls.js";
+import { arrayRandomizer } from "./arrayRandomizer.js";
 
 let array1 = generateArray(10);
+arrayRandomizer(array1);
 for (let i = 0; i < array1.length; i++) {
   for (let j = 0; j < array1[i].length; j++) {
-    let wallCheck = checkForWalls(array1);
-    array1[i][j] = floor(Math.random(2));
+    const wallCheck = checkForWalls(array1);
     if (wallCheck === 0) {
-      neighborCheck = checkForNeighbors(
+      const neighborCheck = checkForNeighbors(
         array1,
         array1.length - 1,
         array1[i].length - 1
--- arrayRandomizer.js
+++ arrayRandomizer.js
@@ -1,15 +1,9 @@
 export const arrayRandomizer = (arr) => {
-  for (let i = 0; arr.length; i++) {
-    for (let j = 0; arr[i].length; j++) {
-      arr[i][j] = Math.floor(Math.random(2));
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      arr[i][j] = Math.floor(Math.random() * 2);
     }
   }
-};
 
-console.log(
-  arrayRandomizer([
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-  ])
-);
+  return arr;
+};
